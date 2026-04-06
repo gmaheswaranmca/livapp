b8acf4db4801a1957a19b8dcaeba91a771358d8a
gmaheswaranmca <gmaheswaranmca@gmail.com>
2026-04-06 19:45:20 +0530

    LivApp1-8

---
+++ z0001_v0/01/server/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.TRAINER_APP_PORT || 5000;
+const MONGO_URI = process.env.TRAINER_MONGO_URI || "";
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Mongo connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
